393cc5c5de12e2bf5999841029e1af2175090bf2
tsingson <tsingson@gmail.com>
2026-07-29 11:25:18 +0800

    clean up

--- compiler-go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/hit9/bitproto/compiler-go
-
-go 1.22
--- compiler-go/main.go
+++ compiler-go/main.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/hit9/bitproto/compiler-go/native"
+	"github.com/tsingson/bitproto/compiler-go/native"
 )
 
 func main() {
